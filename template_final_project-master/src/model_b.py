d806bab8be3ccd222d97b9adb16d614dad92193f
avagracee <69159669+avagracee@users.noreply.github.com>
2024-11-20 19:19:52 -0500

    Create model_b.py

---
+++ template_final_project-master/src/model_b.py
@@ -0,0 +1,25 @@
+class Fisherman:
+def __init__(self, x, y, img_file):
+""" Initializes the fisherman
+args:
+- x : int - starting x coordinate
+- y : int - starting y coordinate
+- img_file : str - path to img file
+"""
+def move_right(self):
+""" moves position right by 1
+args: None
+return: None
+"""
+def move_left(self):
+""" moves position left by 1
+args: None
+return: None
+"""
+def tryFish(self):
+""" extends the fishing line into the water directly below the fisherman
+args: 
+- if True, the fish counter updates by 1
+- if False, the fish counter does not update
+return: Fish
+"""
