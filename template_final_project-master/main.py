7c521c0769e6008c9f0434481eab21a5e1d0247e
avagracee <69159669+avagracee@users.noreply.github.com>
2024-12-04 20:21:25 -0500

    Update main.py

--- template_final_project-master/main.py
+++ template_final_project-master/main.py
@@ -1,10 +1,10 @@
 import pygame
-#import your controller
+from controller import GameController
 
 def main():
     pygame.init()
-    #Create an instance on your controller object
-    #Call your mainloop
+    game_controller = GameController()
+    game_controller.run()
     
     ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 3 LINES OF CODE ######
 
