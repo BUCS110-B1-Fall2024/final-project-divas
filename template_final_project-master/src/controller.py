b91f2c647a3134fd48997062ece1c25a4dd6f293
avagracee <69159669+avagracee@users.noreply.github.com>
2024-12-05 23:38:42 -0500

    Update controller.py

--- template_final_project-master/src/controller.py
+++ template_final_project-master/src/controller.py
@@ -18,33 +18,6 @@ Green = (0, 255, 0)
 White = (255, 255, 255)
 Blue = (0, 0, 255)
 Lightblue = (173, 216, 230)
-
-class Mermaid:
-    """
-    Represents the mermaid
-    x(int): The mermaid's x-coordinates
-    y(int): The mermaid's y-coordinates
-    width(int): The width of the mermaid
-    height(int): The height of the mermaid
-    speed(int): How fast the mermaid moves
-    rect(pygame.Rect): The rectangle representing the mermaid's position
-    
-    """
-    def __init__(self, x, y, width, height, speed):
-        """
-        Initializes the mermaid
-        x(int): The mermaid's starting x-coordinate
-        y(int): The mermaid's starting y-coordinate
-        width(int): The mermaid's width
-        height(int): The mermaid's height
-        speed(int): The mermaid's speed
-        """
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.speed = speed
-        self.rect = pygame.Rect(x, y, width, height)
     
     def move(self,keys):
         """
